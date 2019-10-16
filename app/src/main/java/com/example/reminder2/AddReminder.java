0e7fc2670c04a8412b7105bfa5babcaf2a2ce945
ali <alisamymohamed@yahoo.com>
2019-10-16 04:06:29 -0700

    time and date

--- app/src/main/java/com/example/reminder2/AddReminder.java
+++ app/src/main/java/com/example/reminder2/AddReminder.java
@@ -7,13 +7,9 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,7 +26,6 @@ import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -38,17 +33,17 @@ import java.util.Date;
 public class AddReminder extends BaseActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener, View.OnClickListener {
 
 
-    private EditText mTitle , mDecription , mDate , mTime;
-    private TextView mSetdate , mSetTime;
+    private EditText mTitle, mDecription, mDate, mTime;
+    private TextView mSetdate, mSetTime;
     private ImageView mImage;
-    private Button mAdd , mLocation , mSave;
+    private Button mAdd, mLocation, mSave;
 
-    private int mHour,mYear,mMonth , mMinute , mDay;
-    private String sTime , sDate;
+    private int mHour, mYear, mMonth, mMinute, mDay;
+    private String sTime, sDate;
     private MyLocationProvider myLocationProvider;
     private Location location;
-    private double lat,lang;
-    int  Request_Camera=100 , Select_Image=101;
+    private double lat, lang;
+    int Request_Camera = 100, Select_Image = 101;
     public byte[] byteImage;
 
     @Override
@@ -65,68 +60,30 @@ public class AddReminder extends BaseActivity implements DatePickerDialog.OnDate
         String currentTime = sdf2.format(new Date());
         mSetTime.setText(currentTime);
 
-
-
-
     }
 
     private void initView() {
-        mTitle =  findViewById(R.id.title);
+        mTitle = findViewById(R.id.title);
         mDecription = findViewById(R.id.decription);
-        mDate =  findViewById(R.id.date);
-        mSetdate =  findViewById(R.id.setdate);
+        mDate = findViewById(R.id.date);
+        mSetdate = findViewById(R.id.setdate);
+        mTime = findViewById(R.id.time);
         mSetdate.setOnClickListener(this);
-        mTime =  findViewById(R.id.time);
-        mSetTime =  findViewById(R.id.setTime);
+        mSetTime = findViewById(R.id.setTime);
         mSetTime.setOnClickListener(this);
-        mImage =  findViewById(R.id.image);
-        mAdd =  findViewById(R.id.add);
+
+        mImage = findViewById(R.id.image);
+        mAdd = findViewById(R.id.add);
         mAdd.setOnClickListener(this);
-        mLocation =  findViewById(R.id.location);
+        mLocation = findViewById(R.id.location);
         mLocation.setOnClickListener(this);
-        mSave =  findViewById(R.id.save);
+        mSave = findViewById(R.id.save);
         mSave.setOnClickListener(this);
     }
 
 
-    public void setDate(View v) {
 
-    }
 
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        monthOfYear++;
-        mDay = dayOfMonth;
-        mMonth = monthOfYear;
-        mYear = year;
-        sDate = dayOfMonth + "/" + monthOfYear + "/" + year;
-        Toast.makeText(this, sDate, Toast.LENGTH_SHORT).show();
-        mSetdate.setText(sDate);
-    }
-
-    public void setTime(View v) {
-        Calendar now = Calendar.getInstance();
-        TimePickerDialog tpd = TimePickerDialog.newInstance(
-                this,
-                now.get(Calendar.HOUR_OF_DAY),
-                now.get(Calendar.MINUTE),
-                false
-        );
-        tpd.setThemeDark(false);
-        tpd.show(getFragmentManager(), "Timepickerdialog");
-    }
-
-    @Override
-    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
-        mHour = hourOfDay;
-        mMinute = minute;
-        if (minute < 10) {
-            sTime = hourOfDay + ":" + "0" + minute;
-        } else {
-            sTime = hourOfDay + ":" + minute;
-        }
-        mSetTime.setText(sTime);
-    }
 
    /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -159,22 +116,22 @@ public class AddReminder extends BaseActivity implements DatePickerDialog.OnDate
 
     private void saveReminder() {
         String titleS = mTitle.getText().toString();
-        String des=mDecription.getText().toString();
+        String des = mDecription.getText().toString();
         String date = mSetdate.getText().toString();
         String timeS = mSetTime.getText().toString();
 
-        if(mImage.getDrawable()!=null){
+        if (mImage.getDrawable() != null) {
 
             Bitmap bitmap = ((BitmapDrawable) mImage.getDrawable()).getBitmap();
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            if(bitmap !=null) {
+            if (bitmap != null) {
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
                 byteImage = baos.toByteArray();
 
             }
         }
 
-        Note note = new Note(titleS,des ,date ,timeS,lat,lang,byteImage);
+        Note note = new Note(titleS, des, date, timeS, lat, lang, byteImage);
         NoteDataBase.getInstance(this).notesDao()
                 .insert(note);
         showMessage(R.string.note_added_successfully, R.string.ok
@@ -190,19 +147,6 @@ public class AddReminder extends BaseActivity implements DatePickerDialog.OnDate
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.setdate:
-                Calendar now = Calendar.getInstance();
-                DatePickerDialog dpd = DatePickerDialog.newInstance(
-                        this,
-                        now.get(Calendar.YEAR),
-                        now.get(Calendar.MONTH),
-                        now.get(Calendar.DAY_OF_MONTH)
-                );
-                dpd.show(getFragmentManager(), "Datepickerdialog");
-                break;
-            case R.id.setTime:
-
-                break;
             case R.id.add:
                 SelectImage();
                 break;
@@ -212,22 +156,75 @@ public class AddReminder extends BaseActivity implements DatePickerDialog.OnDate
                     myLocationProvider = new MyLocationProvider(this);
                 // lw msh 3awaz a listen 3la update ab3t null
                 location = myLocationProvider.getCurrentLocation(null);
-                lat=location.getLatitude();
-                lang=location.getLongitude();
+                lat = location.getLatitude();
+                lang = location.getLongitude();
                 break;
             case R.id.save:
                 // TODO 19/10/15
                 saveReminder();
-
+                break;
+            case R.id.setdate:// TODO 19/10/16
+                setDate();
+                break;
+            case R.id.setTime:// TODO 19/10/16
+                setTime();
                 break;
             default:
                 break;
         }
     }
 
-    private void SelectImage(){
+    public void setTime() {
+        Calendar now = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(
+                this,
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                false
+        );
+        tpd.setThemeDark(false);
+        tpd.show(getFragmentManager(), "Timepickerdialog");
+    }
 
-        final CharSequence[] items ={"Camera","Gallery","Cancel"};
+    public void setDate() {
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        dpd.show(getFragmentManager(), "Datepickerdialog");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        monthOfYear++;
+        mDay = dayOfMonth;
+        mMonth = monthOfYear;
+        mYear = year;
+        sDate = dayOfMonth + "/" + monthOfYear + "/" + year;
+        Toast.makeText(this, sDate, Toast.LENGTH_SHORT).show();
+        mSetdate.setText(sDate);
+    }
+
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        mHour = hourOfDay;
+        mMinute = minute;
+        if (minute < 10) {
+            sTime = hourOfDay + ":" + "0" + minute;
+        } else {
+            sTime = hourOfDay + ":" + minute;
+        }
+        mSetTime.setText(sTime);
+    }
+
+
+    private void SelectImage() {
+
+        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(AddReminder.this);
         builder.setTitle("Add Image");
@@ -235,19 +232,19 @@ public class AddReminder extends BaseActivity implements DatePickerDialog.OnDate
             @Override
             public void onClick(DialogInterface dialog, int i) {
 
-                if(items[i].equals("Camera")){
+                if (items[i].equals("Camera")) {
 
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent,Request_Camera);
+                    startActivityForResult(intent, Request_Camera);
 
 
-                }else if (items[i].equals("Gallery")){
+                } else if (items[i].equals("Gallery")) {
 
-                    Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-                    startActivityForResult(intent.createChooser(intent,"Select File"),Select_Image);
+                    startActivityForResult(intent.createChooser(intent, "Select File"), Select_Image);
 
-                }else  if(items[i].equals("Cancel")){
+                } else if (items[i].equals("Cancel")) {
 
                     dialog.dismiss();
 
@@ -258,22 +255,23 @@ public class AddReminder extends BaseActivity implements DatePickerDialog.OnDate
         builder.show();
 
     }
+
     @Override
-    public void onActivityResult(int requestCode,int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode== Activity.RESULT_OK){
+        if (resultCode == Activity.RESULT_OK) {
 
-            if(requestCode== Request_Camera){
+            if (requestCode == Request_Camera) {
 
 
                 Bundle bundle = data.getExtras();
                 final Bitmap bitmap = (Bitmap) bundle.get("data");
                 mImage.setImageBitmap(bitmap);
 
-            }else if(requestCode== Select_Image){
+            } else if (requestCode == Select_Image) {
 
                 Uri SelectedImageUri = data.getData();
                 mImage.setImageURI(SelectedImageUri);
